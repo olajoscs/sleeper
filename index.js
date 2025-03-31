5415f53e3abfb96049c5efc82ddd0e3c4986c57d
Csaba Olajos <olajoscs@gmail.com>
2025-03-31 17:33:08 +0200

    Implement qbittorrent restart + refactor restarting code

--- index.js
+++ index.js
@@ -16,6 +16,57 @@ function renderResponse(res, template, data = {}) {
 	res.render(path.join(__dirname, '/templates', `/${template}`), data);
 }
 
+function restartProcess(appProperties) {
+	let taskName = appProperties.taskName;
+	let exePath = appProperties.exePath;
+	let processFound = null;
+
+	let commandResult;
+	try {
+		commandResult = execSync('tasklist | findstr "' + taskName + '"');
+		processFound = true;
+	} catch(e) {
+		console.log('no process found');
+		processFound = false;
+	}
+
+	if (processFound) {
+		killProcess(commandResult);
+	}
+
+	console.log(exePath);
+	console.log('start "" ""' + exePath + '""');
+
+	setTimeout(
+		() => {
+			execSync('start "" "' + exePath + '"', {stdio: 'inherit', shell: true});
+		},
+		3000
+	);
+}
+
+function killProcess(commandResult) {
+	let processes = commandResult
+		.toString()
+		.split("\n")
+		.map(str => str.trim())
+		.filter(str => !!str);
+
+	let processIds = processes
+		.map(str => {
+			return str
+				.split(' ')
+				.filter(v => !!v)
+				.filter(v => /^\d+$/.test(v))
+		})
+		.map(arr => arr[0])
+	;
+
+	for (let processId of processIds) {
+		execSync('taskkill /PID '+processId);
+	}
+}
+
 app.get('/', function(req, res) {
     renderResponse(res, 'main.html');
 });
@@ -54,46 +105,29 @@ app.get('/restart-plex', function(req, res) {
 	// render loading screen
 	renderResponse(res, 'restart-plex.html');
 
-	// restart plex
-	let commandResult;
-	try {
-		commandResult = execSync('tasklist | findstr "Plex\\ Media\ Server"');
-	} catch(e) {
-		console.log('no process found'); 
-		return;
-	}
-	
-	let processes = commandResult 
-		.toString()
-		.split("\n")
-		.map(str => str.trim())
-		.filter(str => !!str);
-	
-	let processIds = processes
-		.map(str => {
-            return str
-                .split(' ')
-                .filter(v => !!v)
-                .filter(v => /^\d+$/.test(v))
-        })
-        .map(arr => arr[0])
-	;
-	
-	for (let processId of processIds) {
-		execSync('taskkill /PID '+processId);
-	}
-	
-	setTimeout(
-		() => {
-			execSync('start "" "C:\\Program Files\\Plex\\Plex Media Server\\Plex Media Server.exe"', { stdio: 'inherit', shell: true });
-		},
-		3000
-	);
+	restartProcess({
+		taskName: "Plex\ Media\ Server",
+		exePath: "C:\\Program\ Files\\Plex\\Plex\ Media\ Server\\Plex\ Media\ Server.exe",
+	});
 });
 
 
 app.get('/restart-plex-done', function(req, res) {
 	renderResponse(res, 'restart-plex-done.html');
+});
+
+app.get('/restart-qbittorrent', function (req, res) {
+	// render loading screen
+	renderResponse(res, 'restart-qbittorrent.html');
+
+	restartProcess({
+		taskName: 'qbittorrent',
+		exePath: "C:\\Program\ Files\\qBittorrent\\qbittorrent.exe",
+	});
+});
+
+app.get('/restart-qbittorrent-done', function (req, res) {
+	renderResponse(res, 'restart-qbittorrent-done.html');
 });
 
 app.listen(3000);
